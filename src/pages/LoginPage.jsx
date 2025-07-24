c4a22c7799fb296469a337c753fb9ab2fe0b1a06
Mallangc <cky9001@gmail.com>
2025-07-24 23:18:21 +0900

    main / 리팩토링 - 로고, 구글 아이콘 안나오는 문제 수정 - 이미지를 import해서 값 전달

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './LoginPage.css';
+import GoogleLogo from "../../public/google_logo.png"
+import TocktockLogo from "../../public/loginLogoAndText.png"
+
 
 function LoginPage() {
     const handleGoogleLogin = () => {
@@ -10,10 +13,10 @@ function LoginPage() {
 
     return (
         <div className="login-container">
-          <img className="logo" src="../../public/loginLogoAndText.png" alt="loginLogoAndText" sizes="50%"/>
+          <img className="logo" src={TocktockLogo} alt="loginLogoAndText" sizes="50%"/>
           <p className="content">간편한 할일 목록 작성이 <br/> 필요할 때 두잇 톡톡</p>
             <button className="google-login-button" onClick={handleGoogleLogin}>
-              <img src="../../public/google_logo.png" alt="google-logo" className="button-icon"/>
+              <img src={GoogleLogo} alt="google-logo" className="button-icon"/>
                 <span className="button-text">Google로 계속하기</span>
             </button>
           <p className="end-text">로그인 오류 및 문의&nbsp;
